c0c67140347e9b3768f34ef964c3cbc1d5a1491d
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2023-11-15 22:54:39 +0800

    feat: Installed application insights sdk for golang
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- app/main.go
+++ app/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/microsoft/ApplicationInsights-Go/appinsights"
 )
 
 func main() {
+	client := appinsights.NewTelemetryClient(os.Getenv("APPINSIGHTS_INSTRUMENTATIONKEY"))
+	client.TrackEvent("App Connected")
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":8080", nil)
 }
