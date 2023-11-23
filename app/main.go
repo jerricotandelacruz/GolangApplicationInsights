103de9cb469d47f82d9754e780d69069d1e43707
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2023-11-23 22:38:29 +0800

    test: Removed setting of parent id and other context tags except operation id
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- app/main.go
+++ app/main.go
@@ -18,17 +18,7 @@ func main() {
 }
 
 func TrackGroupWithHerierchy() {
-	// SET INSTRUMENTATION KEY
 	client := appinsights.NewTelemetryClient(os.Getenv("APPINSIGHTS_INSTRUMENTATIONKEY"))
-
-	// Set role instance name globally -- this is usually the
-	// name of the service submitting the telemetry
-	client.Context().Tags.Cloud().SetRole("my_go_server")
-
-	// Set the role instance to the host name.  Note that this is
-	// done automatically by the SDK.
-	hostname, _ := os.Hostname()
-	client.Context().Tags.Cloud().SetRoleInstance(hostname)
 
 	client.Context().Tags.Operation().SetId("e49635cc-007d-4006-b661-dd23a5946cf8")
 
@@ -36,18 +26,11 @@ func TrackGroupWithHerierchy() {
 	firstEvent.Properties["property"] = "1STPROPERTY"
 	client.Track(firstEvent)
 
-	client.Context().Tags.Operation().SetParentId(firstEvent.Tags.Operation().GetId())
 	secondEvent := appinsights.NewEventTelemetry("2ND EVENT SUB OF 1ST EVENT")
 	secondEvent.Properties["property"] = "2NDPROPERTY"
 	client.Track(secondEvent)
 
-	for k := range client.Context().Tags.Operation() {
-		delete(client.Context().Tags.Operation(), k)
-	}
-
-	client.Context().Tags.Operation().SetId("e49635cc-007d-4006-b661-dd23a5946cf8")
-
 	thirdEvent := appinsights.NewEventTelemetry("3RD EVENT")
-	thirdEvent.Properties["property"] = "2NDPROPERTY"
+	thirdEvent.Properties["property"] = "3RDPROPERTY"
 	client.Track(thirdEvent)
 }
