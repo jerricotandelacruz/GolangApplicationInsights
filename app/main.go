43c8e972e1443172b692c6253af50dc05b8946da
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2023-11-23 22:53:29 +0800

    refactor: Set different operation id and parent id
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- app/main.go
+++ app/main.go
@@ -20,15 +20,21 @@ func main() {
 func TrackGroupWithHerierchy() {
 	client := appinsights.NewTelemetryClient(os.Getenv("APPINSIGHTS_INSTRUMENTATIONKEY"))
 
-	client.Context().Tags.Operation().SetId("e49635cc-007d-4006-b661-dd23a5946cf5")
+	client.Context().Tags.Operation().SetId("e49635cc-007d-4006-b661-dd23a5946ca5")
 
 	firstEvent := appinsights.NewEventTelemetry("1ST EVENT")
 	firstEvent.Properties["property"] = "1STPROPERTY"
 	client.Track(firstEvent)
 
+	client.Context().Tags.Operation().SetId("e49635cc-007d-4006-b661-dd23a5946cb5")
+	client.Context().Tags.Operation().SetParentId("e49635cc-007d-4006-b661-dd23a5946ca5")
+
 	secondEvent := appinsights.NewEventTelemetry("2ND EVENT SUB OF 1ST EVENT")
 	secondEvent.Properties["property"] = "2NDPROPERTY"
 	client.Track(secondEvent)
+
+	client.Context().Tags.Operation().SetId("e49635cc-007d-4006-b661-dd23a5946cc5")
+	client.Context().Tags.Operation().SetParentId("e49635cc-007d-4006-b661-dd23a5946cc5")
 
 	thirdEvent := appinsights.NewEventTelemetry("3RD EVENT")
 	thirdEvent.Properties["property"] = "3RDPROPERTY"
